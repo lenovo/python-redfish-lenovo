872f8a132e50dbea0af4f5f69efc7472f4072d72
renxulei <renxl2@lenovo.com>
2019-02-27 11:53:10 +0800

    Add get chassis indicator script

---
+++ examples/get_chassis_indicator_led.py
@@ -0,0 +1,111 @@
+###
+#
+# Lenovo Redfish examples - get chassis indicator led
+# Copyright Notice:
+#
+# Copyright 2018 Lenovo Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+###
+
+
+import redfish
+import sys
+import json
+import lenovo_utils as utils
+
+
+def get_chassis_indicator_led(ip, login_account, login_password):
+    """Get chassis indicator led
+    :params ip: BMC IP address
+    :type ip: string
+    :params login_account: BMC user name
+    :type login_account: string
+    :params login_password: BMC user password
+    :type login_password: string
+    :returns: returns get chassis indicator led status when succeeded or error message when failed
+    """
+    result = {}
+    login_host = "https://" + ip
+    try:
+        # Connect using the BMC address, account name, and password
+        # Create a REDFISH object
+        REDFISH_OBJ = redfish.redfish_client(base_url=login_host, username=login_account,
+                                         password=login_password, default_prefix='/redfish/v1')
+        # Login into the server and create a session
+        REDFISH_OBJ.login(auth="session")
+    except:
+        result = {'ret': False, 'msg': "Please check the username, password, IP is correct\n"}
+        return result
+
+    try:
+        # Get ComputerBase resource
+        response_base_url = REDFISH_OBJ.get('/redfish/v1', None)
+        # Get response_base_url
+        if response_base_url.status == 200:
+            chassis_url = response_base_url.dict['Chassis']['@odata.id']
+        else:
+            error_message = utils.get_extended_error(response_base_url)
+            result = {'ret': False, 'msg': "Url '/redfish/v1' response Error code %s \nerror_message: %s" % (
+            response_base_url.status, error_message)}
+            return result
+
+        # Get response chassis url resource
+        response_chassis_url = REDFISH_OBJ.get(chassis_url, None)
+        if response_chassis_url.status == 200:
+            # Get the LED status
+            for i in range(response_chassis_url.dict['Members@odata.count']):
+                led_status = {}
+                led_url = response_chassis_url.dict['Members'][i]['@odata.id']
+                response_led_url = REDFISH_OBJ.get(led_url, None)
+                if response_led_url.status == 200:
+                    if "ComputerSystems" in response_led_url.dict["Links"]:
+                        led_status["indicatorLED"] = response_led_url.dict['IndicatorLED']
+                        result = {'ret': True, 'msg': led_status}
+                        return result
+                else:
+                    error_message = utils.get_extended_error(response_led_url)
+                    result = {'ret': False, 'msg': "Url '%s' response Error code %s \nerror_message: %s" % (
+                        led_url, response_led_url.status, error_message)}
+                    return result
+        else:
+            error_message = utils.get_extended_error(response_chassis_url)
+            result = {'ret': False, 'msg': "Url '%s' response Error code %s \nerror_message: %s" % (chassis_url, response_chassis_url.status, error_message)}
+            return result
+
+    except Exception as e:
+        result = {'ret': False, 'msg': "error_message: %s" % e}
+    finally:
+        # Logout of the current session
+        REDFISH_OBJ.logout()
+        return result
+
+
+if __name__ == '__main__':
+    # Get parameters from config.ini and/or command line
+    argget = utils.create_common_parameter_list()
+    args = argget.parse_args()
+    parameter_info = utils.parse_parameter(args)
+
+    # Get connection info from the parameters user specified
+    ip = parameter_info['ip']
+    login_account = parameter_info["user"]
+    login_password = parameter_info["passwd"]
+
+    # Get chassis indicator led and check result
+    result = get_chassis_indicator_led(ip, login_account, login_password)
+    if result['ret'] is True:
+        del result['ret']
+        sys.stdout.write(json.dumps(result['msg'], sort_keys=True, indent=2))
+    else:
+        sys.stderr.write(result['msg'])
