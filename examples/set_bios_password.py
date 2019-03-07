074fa498eed7ed97aa4b4f671ca72e3d523dd90a
renxulei <renxl2@lenovo.com>
2019-03-07 09:40:09 +0800

    Add handling for 204 code and add extended error info

--- examples/set_bios_password.py
+++ examples/set_bios_password.py
@@ -1,6 +1,6 @@
 ###
 #
-# Lenovo Redfish examples - Get Bios attribute
+# Lenovo Redfish examples - Set bios password
 #
 # Copyright Notice:
 #
@@ -28,7 +28,7 @@ import lenovo_utils as utils
 
 
 def set_bios_password(ip, login_account, login_password, system_id, bios_password_name, bios_password):
-    """Set Bios attribute    
+    """Set Bios password
     :params ip: BMC IP address
     :type ip: string
     :params login_account: BMC user name
@@ -56,55 +56,72 @@ def set_bios_password(ip, login_account, login_password, system_id, bios_passwor
         result = {'ret': False, 'msg': "Please check the username, password, IP is correct"}
         return result
 
-    # GET the ComputerSystem resource
-    system = utils.get_system_url("/redfish/v1",system_id, REDFISH_OBJ)
-    if not system:
-        result = {'ret': False, 'msg': "This system id is not exist or system member is None"}
+    try:
+        # GET the ComputerSystem resource
+        system = utils.get_system_url("/redfish/v1",system_id, REDFISH_OBJ)
+        if not system:
+            result = {'ret': False, 'msg': "This system id is not exist or system member is None"}
+            return result
+
+        for i in range(len(system)):
+            system_url = system[i]
+            response_system_url = REDFISH_OBJ.get(system_url, None)
+            if response_system_url.status == 200:
+                # Get the ComputerBios resource
+                bios_url = response_system_url.dict['Bios']['@odata.id']
+            else:
+                error_message = utils.get_extended_error(response_system_url)
+                result = {'ret': False, 'msg': "Url '%s' response Error code %s \nerror_message: %s" % (
+                    system_url, response_system_url.status, error_message)}
+                return result
+
+            response_bios_url = REDFISH_OBJ.get(bios_url, None)
+            if response_bios_url.status == 200:
+                # Get the change password url
+                change_password_url = response_bios_url.dict['Actions']['#Bios.ChangePassword']['target']
+
+                # Set Password info
+                PasswordName = bios_password_name
+                new_password = bios_password
+                parameter = {"PasswordName":PasswordName, "NewPassword":new_password}
+
+                # Change password
+                response_change_password = REDFISH_OBJ.post(change_password_url, body=parameter)
+                if response_change_password.status in [200, 204]:
+                    result = {'ret': True, 'msg': 'Setting BIOS password successfully'}
+                else:
+                    error_message = utils.get_extended_error(response_change_password)
+                    result = {'ret': False, 'msg': "Url '%s' response Error code %s \nerror_message: %s" % (
+                        change_password_url, response_change_password.status, error_message)}
+                    return result
+            else:
+                error_message = utils.get_extended_error(response_bios_url)
+                result = {'ret': False, 'msg': "Url '%s' response Error code %s \nerror_message: %s" % (
+                    bios_url, response_bios_url.status, error_message)}
+                return result
+
+    except Exception as e:
+        result = {'ret': False, 'msg': "error_message: %s" % (e)}
+    finally:
+        # Logout of the current session
         REDFISH_OBJ.logout()
         return result
-    for i in range(len(system)):
-        system_url = system[i]
-        response_system_url = REDFISH_OBJ.get(system_url, None)
-        if response_system_url.status == 200:
-            # Get the ComputerBios resource
-            bios_url = response_system_url.dict['Bios']['@odata.id']
-        else:
-            result = {'ret': False, 'msg': "response system url Error code %s" % response_system_url.status}
-            REDFISH_OBJ.logout()
-            return result
-        response_bios_url = REDFISH_OBJ.get(bios_url, None)
-        if response_bios_url.status == 200:
-            # Get the change password url
-            change_password_url = response_bios_url.dict['Actions']['#Bios.ChangePassword']['target']
-            # Set Password info
-            PasswordName = bios_password_name
-            new_password = bios_password
-            parameter = {"PasswordName":PasswordName, "NewPassword":new_password}
-            # Change password
-            response_change_password = REDFISH_OBJ.post(change_password_url, body=parameter)
-            if response_change_password.status == 200:
-                result = {'ret': True, 'msg': 'set bios password successful'}
-            else:
-                result = {'ret': False, 'msg': 'response change password Error code %s'% response_change_password.status}
-                REDFISH_OBJ.logout()
-                return result
-        else:
-            result = {'ret': False, 'msg': "response bios url Error code %s" % response_bios_url.status}
-            REDFISH_OBJ.logout()
-            return result
-    # Logout of the current session
-    REDFISH_OBJ.logout()
-    return result
 
 
 import argparse
+def add_helpmessage(parser):
+    parser.add_argument('--name', type=str, required=True, help='Input the bios password name ("UefiAdminPassword" or "UefiPowerOnPassword")')
+    parser.add_argument('--biospasswd', type=str, required=True, help='Input the new bios password')
+
+
 def add_parameter():
     """Add set bios password parameter"""
     argget = utils.create_common_parameter_list()
-    argget.add_argument('--name', type=str, help='Input the bios password name("UefiAdminPassword" or "UefiPowerOnPassword")')
-    argget.add_argument('--biospasswd', type=str, help='Input the new bios password')
+    add_helpmessage(argget)
     args = argget.parse_args()
     parameter_info = utils.parse_parameter(args)
+    parameter_info['bios_password_name'] = args.name
+    parameter_info['bios_password'] = args.biospasswd
     return parameter_info
 
 
