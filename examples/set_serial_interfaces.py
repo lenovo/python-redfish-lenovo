ef9bc64ee34a9f96ad3dfb3a64aa573e63760d7c
renxulei <renxl2@lenovo.com>
2019-12-27 20:08:49 +0800

    fix bugs to specify correct interface id.

--- examples/set_serial_interfaces.py
+++ examples/set_serial_interfaces.py
@@ -56,49 +56,40 @@ def set_serial_interfaces(ip, login_account, login_password, interfaceid, bitrat
     
         # Login into the server and create a session
         REDFISH_OBJ.login(auth="session")
-    except:
-        result = {'ret': False, 'msg': "Please check the username, password, IP is correct"}
+    except Exception as e:
+        result = {'ret': False, 'msg': "Error_message: %s. Please check if username, password and IP are correct" % repr(e)}
         return result
+        
     try:
         base_response = REDFISH_OBJ.get('/redfish/v1', None)
         if base_response.status == 200:
-            # get the managers url
+            # Get the managers url
             managers_url = base_response.dict['Managers']['@odata.id']
         else:
-            try:
-                error_message = utils.get_extended_error(base_response)
-            except:
-                error_message = base_response
-            result = {'ret': False, 'msg': "response base url Error code %s \nerror_message: %s" % (base_response.status, error_message)}
-            
+            error_message = utils.get_extended_error(base_response)
+            result = {'ret': False, 'msg': "Url '/redfish/v1' response Error code %s \nerror_message: %s" % (base_response.status, error_message)}
             return result
 
         managers_url_response = REDFISH_OBJ.get(managers_url, None)
         if managers_url_response.status == 200:
-            # get the managers url collection
+            # Get the managers url collection
             managers_url_collection = managers_url_response.dict['Members']
         else:
-            try:
-                error_message = utils.get_extended_error(managers_url_response)
-            except:
-                error_message = managers_url_response
-            result = {'ret': False, 'msg': "response managers_url Error code %s \nerror_message: %s" % (managers_url_response.status, error_message)}
-            
+            error_message = utils.get_extended_error(managers_url_response)
+            result = {'ret': False, 'msg': "Url '%s' response Error code %s \nerror_message: %s" % (managers_url, managers_url_response.status, error_message)}
             return result
 
+        id_found = False
         # Get the manager url from managers url collection
-        for i in managers_url_collection:
-            manager_x_url = i["@odata.id"]
-            manager_x_url_response = REDFISH_OBJ.get(manager_x_url, None)
-            if manager_x_url_response.status == 200:
+        for manager in managers_url_collection:
+            manager_url = manager["@odata.id"]
+            manager_url_response = REDFISH_OBJ.get(manager_url, None)
+            if manager_url_response.status == 200:
                 # Get the serial interfaces url
-                serial_interfaces_url = manager_x_url_response.dict['SerialInterfaces']['@odata.id']
+                serial_interfaces_url = manager_url_response.dict['SerialInterfaces']['@odata.id']
             else:
-                try:
-                    error_message = utils.get_extended_error(manager_x_url_response)
-                except:
-                    error_message = manager_x_url_response
-                result = {'ret': False, 'msg': "response manager_x_url Error code %s \nerror_message: %s" % (manager_x_url_response.status, error_message)}
+                error_message = utils.get_extended_error(manager_url_response)
+                result = {'ret': False, 'msg': "Url '%s' response Error code %s \nerror_message: %s" % (manager_url, manager_url_response.status, error_message)}
                 return result
 
             # Get the serial interfaces url collection
@@ -106,87 +97,79 @@ def set_serial_interfaces(ip, login_account, login_password, interfaceid, bitrat
             if serial_interfaces_url_response.status == 200:
                 serial_interfaces_url_collection = serial_interfaces_url_response.dict['Members']
             else:
-                try:
-                   error_message = utils.get_extended_error(serial_interfaces_url_response)
-                except:
-                   error_message = serial_interfaces_url_response
-                result = {'ret': False, 'msg': "response serial_interfaces_url_response Error code %s \nerror_message: %s" % (serial_interfaces_url_response.status, error_message)}
+                error_message = utils.get_extended_error(serial_interfaces_url_response)
+                result = {'ret': False, 'msg': "Url '%s' response Error code %s \nerror_message: %s" % (serial_interfaces_url, serial_interfaces_url_response.status, error_message)}
                 return result
 
-            # Get the serial interfaces url form serial interfaces url collection
-            try:
-                # check interface id invalidity
-                serial_interfaces_x_url = ""
-                # treat input interfaceid as resource id string
-                for interface_member in serial_interfaces_url_collection:
-                    interface_name = interface_member['@odata.id'].split("/")[-1]
-                    if interfaceid == interface_name:
-                        serial_interfaces_x_url = interface_member['@odata.id']
-                        break
-                # treat input interfaceid as sequence number
-                if serial_interfaces_x_url == "" and interfaceid.isdigit():
-                    index = int(interfaceid) - 1
-                    if index >= 0 and index < len(serial_interfaces_url_collection):
-                        serial_interfaces_x_url = serial_interfaces_url_collection[index]['@odata.id']
-                # invalid interfaceid
-                if(serial_interfaces_x_url == ""):
-                    result = {'ret': False, 'msg': "The specified Interface Id does not exist."}
+            # go through all serial interafces to find the interface specified by interfaceid, then change the setting.
+            for serial_interafce in serial_interfaces_url_collection:
+                serial_interface_url = serial_interafce['@odata.id']
+                # Get single serial interface info
+                response_serial_interface_url = REDFISH_OBJ.get(serial_interface_url, None)
+                if response_serial_interface_url.status == 200:
+                    if interfaceid == '' or interfaceid == response_serial_interface_url.dict['Id']:
+                        id_found = True
+                        # specify interfaceid as first instance's id.
+                        interfaceid = response_serial_interface_url.dict['Id']
+                        if "@odata.etag" in response_serial_interface_url.dict:
+                            etag = response_serial_interface_url.dict['@odata.etag']
+                        else:
+                            etag = ""
+                        headers = {"If-Match": etag}
+
+                        body = {}
+                        if bitrate:
+                            if "BitRate" not in response_serial_interface_url.dict:
+                                result = {'ret': False, 'msg': "No BitRate in interface %s." %(interfaceid)}
+                                return result
+                            body['BitRate'] = bitrate
+
+                        if parity:
+                            if "Parity" not in response_serial_interface_url.dict:
+                                result = {'ret': False, 'msg': "No Parity in interface %s." %(interfaceid)}
+                                return result
+                            body['Parity'] = parity
+                            
+                        if stopbits:
+                            if "StopBits" not in response_serial_interface_url.dict:
+                                result = {'ret': False, 'msg': "No StopBits in interface %s." %(interfaceid)}
+                                return result                        
+                            body['StopBits'] = stopbits
+                            
+                        if enabled:
+                            if "InterfaceEnabled" not in response_serial_interface_url.dict:
+                                result = {'ret': False, 'msg': "No InterfaceEnabled in interface %s." %(interfaceid)}
+                                return result                        
+                            body['InterfaceEnabled'] = bool(int(enabled))
+
+                        # Set serial interface
+                        set_serial_interface_response = REDFISH_OBJ.patch(serial_interface_url, body=body, headers=headers)
+                        if set_serial_interface_response.status in [200,204]:
+                            result = {'ret': True, 'msg': 'Set %s successfully'% [key+':'+str(value) for key, value in body.items()]}
+                        else:
+                            error_message = utils.get_extended_error(set_serial_interface_response)
+                            result = {'ret': False, 'msg': "Error code %s \nerror_message: %s" % (set_serial_interface_response.status, error_message)}
+                else:
+                    error_message = utils.get_extended_error(response_serial_interface_url)
+                    result = {'ret': False, 'msg': "Url '%s' response Error code %s\nerror_message: %s" % (
+                        serial_interface_url, response_serial_interface_url.status, error_message)}
                     return result
 
-                # get etag to set If-Match precondition
-                response_serial_interfaces_x_url = REDFISH_OBJ.get(serial_interfaces_x_url, None)
-                if response_serial_interfaces_x_url.status != 200:
-                    error_message = utils.get_extended_error(response_serial_interfaces_x_url)
-                    result = {'ret': False, 'msg': "Url '%s' get failed. response Error code %s \nerror_message: %s" % (
-                        serial_interfaces_x_url, response_serial_interfaces_x_url.status, error_message)}
-                    return result
-                if "@odata.etag" in response_serial_interfaces_x_url.dict:
-                    etag = response_serial_interfaces_x_url.dict['@odata.etag']
-                else:
-                    etag = ""
-                headers = {"If-Match": etag}
+        if id_found == False:
+            result = {'ret': False, 'msg': "The specified Interface Id %s does not exist." % interfaceid}
+            return result
 
-                # check whether the specified interface is valid
-                if "BitRate" not in response_serial_interfaces_x_url.dict:
-                    result = {'ret': False, 'msg': "The specified Interface Id %s has no BitRate property, not valid." %(interfaceid)}
-                    return result
-                
-                # Build body for setting serial interface properties value
-                body = {}
-                if bitrate:
-                    body['BitRate'] = bitrate
-                if parity:
-                    body['Parity'] = parity
-                if stopbits:
-                    body['StopBits'] = stopbits
-                if enabled:
-                    body['InterfaceEnabled'] = bool(int(enabled))
-                else:
-                    body['InterfaceEnabled'] = False
-                
-                # Request set serial interface
-                serial_interfaces_x_url_response = REDFISH_OBJ.patch(serial_interfaces_x_url, body=body, headers=headers)
-                if serial_interfaces_x_url_response.status in [200,204]:
-                    result = {'ret': True, 'msg': 'Set %s successfully'% [key+':'+str(value) for key, value in body.items()]}
-                else:
-                    try:
-                        error_message = utils.get_extended_error(serial_interfaces_x_url_response)
-                    except:
-                        error_message = serial_interfaces_x_url_response
-                    result = {'ret': False, 'msg': "response serial_interfaces_x_url_response Error code %s \nerror_message: %s" % (serial_interfaces_x_url_response.status, error_message)}
-            except IndexError:
-                result = {'ret': False, 'msg': "The specified Interface Id does not exist."}
     finally:
         # Logout of the current session
         REDFISH_OBJ.logout()
-        return result       
+        return result
 
 
 import argparse
 def add_parameter():
     """Add set serial interfaces attribute parameter"""
     argget = utils.create_common_parameter_list()
-    argget.add_argument('--interfaceid', type=str, default='1', help='Serial interface instance id. (default instance id is 1)')
+    argget.add_argument('--interfaceid', type=str, default='', help='Serial interface instance id. (default is first instance\'s id)')
     argget.add_argument('--bitrate', type=str, default='', help='This property indicates the transmit and receive speed of the serial connection. Support: [9600, 19200, 38400, 57600, 115200]')
     argget.add_argument('--stopbits', type=str, default='', help='This property indicates the stop bits for the serial connection. Support:["1","2"].')
     argget.add_argument('--parity', type=str, default='', help='This property indicates parity information for a serial connection. Support: ["None", "Even", "Odd"]')
@@ -232,6 +215,6 @@ if __name__ == '__main__':
         del result['ret']
         sys.stdout.write(json.dumps(result['msg'], sort_keys=True, indent=2))
     else:
-        sys.stderr.write(result['msg'])
+        sys.stderr.write(result['msg'] + '\n')
 
     
