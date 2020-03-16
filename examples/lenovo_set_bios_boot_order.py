ed8c04d26751f7e1e2119cc0ef03d535d35b6a85
panyy3 <panyy3@lenovo.com>
2020-03-16 18:19:16 +0800

    add auth option support in oem bios setting scripts

--- examples/lenovo_set_bios_boot_order.py
+++ examples/lenovo_set_bios_boot_order.py
@@ -26,7 +26,7 @@ import redfish
 import lenovo_utils as utils
 
 
-def set_bios_boot_order(ip, login_account, login_password, system_id, bootorder):
+def set_bios_boot_order(ip, login_account, login_password, system_id, auth, bootorder):
     """set bios boot order
     :params ip: BMC IP address
     :type ip: string
@@ -36,6 +36,8 @@ def set_bios_boot_order(ip, login_account, login_password, system_id, bootorder)
     :type login_password: string
     :params system_id: ComputerSystem instance id(None: first instance, All: all instances)
     :type system_id: None or string
+    :params auth: Authentication mode(session or basic)
+    :type auth: string
     :params bootorder: Specify the bios boot order list,  The boot order takes effect on the next startup
     :type bootorder: list
     :returns: returns set bios boot order result when succeeded or error message when failed
@@ -48,7 +50,7 @@ def set_bios_boot_order(ip, login_account, login_password, system_id, bootorder)
         REDFISH_OBJ = redfish.redfish_client(base_url=login_host, username=login_account,
                                              password=login_password, default_prefix='/redfish/v1')
         # Login into the server and create a session
-        REDFISH_OBJ.login(auth="session")
+        REDFISH_OBJ.login(auth=auth)
     except:
         result = {'ret': False, 'msg': "Please check the username, password, IP is correct"}
         return result
@@ -133,6 +135,7 @@ if __name__ == '__main__':
     login_account = parameter_info["user"]
     login_password = parameter_info["passwd"]
     system_id = parameter_info["sysid"]
+    auth = parameter_info['auth']
 
     # Get set info from the parameters user specified
     try:
@@ -142,7 +145,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     # Get set bios boot order result and check result
-    result = set_bios_boot_order(ip, login_account, login_password, system_id, bootorder)
+    result = set_bios_boot_order(ip, login_account, login_password, system_id, auth, bootorder)
     if result['ret'] is True:
         del result['ret']
         sys.stdout.write(json.dumps(result['msg'], sort_keys=True, indent=2))
