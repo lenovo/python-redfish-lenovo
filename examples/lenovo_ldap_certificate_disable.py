de76260d12b985ee0af920bb776283c05f00fc32
Tami YY3 Pan <panyy3@lenovo.com>
2021-01-22 18:32:40 +0800

    update scripts to support certificate enable/disable for SR635/SR655

--- examples/lenovo_ldap_certificate_disable.py
+++ examples/lenovo_ldap_certificate_disable.py
@@ -4,7 +4,7 @@
 #
 # Copyright Notice:
 #
-# Copyright 2020 Lenovo Corporation
+# Copyright 2021 Lenovo Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -25,7 +25,7 @@ import json
 import lenovo_utils as utils
 
 
-def lenovo_ldap_certificate_disable(ip, login_account, login_password):
+def lenovo_ldap_certificate_disable(ip, login_account, login_password, binddn=None, bindpassword=None):
     """ Enable LDAP certificate
         :params ip: BMC IP address
         :type ip: string
@@ -33,6 +33,10 @@ def lenovo_ldap_certificate_disable(ip, login_account, login_password):
         :type login_account: string
         :params login_password: BMC user password
         :type login_password: string
+        :params binddn: LDAP DN for binding
+        :type binddn: string
+        :params bindpassword: LDAP password for binding
+        :type bindpassword: string
         :returns: returns get successful result when succeeded or error message when failed
         """
 
@@ -126,6 +130,62 @@ def lenovo_ldap_certificate_disable(ip, login_account, login_password):
                 pass
             return result
 
+        # Disable LDAP for SR635/SR655
+        accountservice_url = "/redfish/v1/AccountService"
+        response_accountservice_url = REDFISH_OBJ.get(accountservice_url, None)
+        if response_accountservice_url.status != 200:
+            error_message = utils.get_extended_error(response_accountservice_url)
+            result = {'ret': False, 'msg': "Url '%s' response Error code %s\nerror_message: %s" % (
+                accountservice_url, response_accountservice_url.status, error_message)}
+            try:
+                REDFISH_OBJ.logout()
+            except:
+                pass
+            return result
+        try:
+            encryption_type = response_accountservice_url.dict['LDAP']['Authentication']['Oem']['Ami']['EncryptionType']
+            if encryption_type == 'NoEncryption':
+                result = {'ret': True,
+                          'msg':"LDAP certificate security is already disabled."}
+                try:
+                    REDFISH_OBJ.logout()
+                except:
+                    pass
+                return result
+
+            elif encryption_type == 'SSL' or encryption_type == 'StartTLS':
+                if binddn is None or bindpassword is None:
+                    result = {'ret': False,
+                              'msg':"Parameter binddn and bindpassword are needed for disabling LDAP certificate security."}
+                    try:
+                        REDFISH_OBJ.logout()
+                    except:
+                        pass
+                    return result
+
+                # Create request body
+                target_url = accountservice_url
+                request_body = {"LDAP": {"Authentication": {"Username": binddn, "Password": bindpassword, "Oem": {"Ami": {"EncryptionType": "NoEncryption", "CommonNameType": "IPAddress"}}}}}
+                headers = {"If-Match": "*"}
+
+                # Perform patch to disable LDAP SSL
+                response_url = REDFISH_OBJ.patch(target_url, body=request_body, headers=headers)
+                if response_url.status not in [200, 201, 202, 204]:
+                    error_message = utils.get_extended_error(response_url)
+                    result = {'ret': False, 'msg': "Url '%s' response Error code %s\nerror_message: %s" % (
+                        target_url, response_url.status, error_message)}
+                else:
+                    result = {'ret': True,
+                              'msg':"LDAP certificate security is disabled."}
+                try:
+                    REDFISH_OBJ.logout()
+                except:
+                    pass
+                return result
+
+        except:
+            pass # Related resource not existing
+
         # No LDAP certificate resource found
         result = {'ret': False, 'msg': 'LDAP certificate is not supported'}
         return result
@@ -140,12 +200,20 @@ def lenovo_ldap_certificate_disable(ip, login_account, login_password):
             pass
 
 
+def add_helpmessage(argget):
+    argget.add_argument('--binddn', type=str, required=False, help='Specify DN for binding LDAP server with a DN and password. If DN is not specified, try to bind anonymous. Only ThinkSystem SR635/SR655 need to specify binddn/bindpassword.')
+    argget.add_argument('--bindpassword', type=str, required=False, help='Specify password for binding LDAP server with a DN and password.i Only ThinkSystem SR635/SR655 need to specify binddn/bindpassword.')
+
+
 def add_parameter():
     """Add parameter"""
     parameter_info = {}
     argget = utils.create_common_parameter_list()
+    add_helpmessage(argget)
     args = argget.parse_args()
     parameter_info = utils.parse_parameter(args)
+    parameter_info["binddn"] = args.binddn
+    parameter_info["bindpassword"] = args.bindpassword
     return parameter_info
 
 
@@ -155,9 +223,11 @@ if __name__ == '__main__':
     ip = parameter_info['ip']
     login_account = parameter_info["user"]
     login_password = parameter_info["passwd"]
+    binddn = parameter_info["binddn"]
+    bindpassword = parameter_info["bindpassword"]
 
     # Enable ldap certificate security and check result
-    result = lenovo_ldap_certificate_disable(ip, login_account, login_password)
+    result = lenovo_ldap_certificate_disable(ip, login_account, login_password, binddn, bindpassword)
     if result['ret'] is True:
         del result['ret']
         sys.stdout.write(json.dumps(result['msg'], sort_keys=True, indent=2))
