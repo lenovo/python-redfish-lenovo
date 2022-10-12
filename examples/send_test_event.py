08d1c3c4a10b1a8114d48ea4ca661e5070d7387c
Bowen BW8 Li <libw8@Lenovo.com>
2022-10-12 15:44:35 +0800

    Fix timestamp issues

--- examples/send_test_event.py
+++ examples/send_test_event.py
@@ -80,7 +80,7 @@ def send_test_event(ip, login_account, login_password,eventid,message,severity):
                     EventService_Version = int(EventService_Type.replace('v','').replace('_',''))
                 # Construct hearders and body to do post
                 target_url = response_event_url.dict["Actions"]["#EventService.SubmitTestEvent"]["target"]
-                timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S+08:00')
+                timestamp = (datetime.datetime.now()+datetime.timedelta(minutes=2)).strftime('%Y-%m-%dT%H:%M:%S+08:00')
                 headers = {"Content-Type": "application/json"}
                 payload = {}
                 if "@Redfish.ActionInfo" in response_event_url.dict["Actions"]["#EventService.SubmitTestEvent"]:
@@ -98,9 +98,9 @@ def send_test_event(ip, login_account, login_password,eventid,message,severity):
                                elif parameter["Name"] == "Message":
                                    payload["Message"] = message
                                elif parameter["Name"] == "MessageArgs":
-                                   payload["MessageArgs"] = []
+                                   payload["MessageArgs"] = ["Lit","IndicatorLED"]
                                elif parameter["Name"] == "MessageId":
-                                   payload["MessageId"] = "Created"
+                                   payload["MessageId"] = "Base.1.5.PropertyValueNotInList"
                                elif parameter["Name"] == "Severity":
                                    payload["Severity"] = severity
                                elif parameter["Name"] == "OriginOfCondition":
@@ -109,15 +109,15 @@ def send_test_event(ip, login_account, login_password,eventid,message,severity):
                     payload["EventId"] = eventid
                     payload["EventTimestamp"] = timestamp
                     payload["Message"] = message
-                    payload["MessageArgs"] = []
-                    payload["MessageId"] = "Created"
+                    payload["MessageArgs"] = ["Lit","IndicatorLED"]
+                    payload["MessageId"] = "Base.1.5.PropertyValueNotInList"
                     payload["OriginOfCondition"] = event_url
                 elif EventService_Version >= 130:
                     payload["EventId"] = eventid
                     payload["EventTimestamp"] = timestamp
                     payload["Message"] = message
-                    payload["MessageArgs"] = []
-                    payload["MessageId"] = "Created"
+                    payload["MessageArgs"] = ["Lit","IndicatorLED"]
+                    payload["MessageId"] = "Base.1.5.PropertyValueNotInList"
                     payload["Severity"] = severity
                     payload["OriginOfCondition"] = event_url
                 elif EventService_Version >= 106:
@@ -125,8 +125,8 @@ def send_test_event(ip, login_account, login_password,eventid,message,severity):
                     payload["EventType"] = "Alert"
                     payload["EventTimestamp"] = timestamp
                     payload["Message"] = message
-                    payload["MessageArgs"] = []
-                    payload["MessageId"] = "Created"
+                    payload["MessageArgs"] = ["Lit","IndicatorLED"]
+                    payload["MessageId"] = "Base.1.5.PropertyValueNotInList"
                     payload["Severity"] = severity
                     payload["OriginOfCondition"] = event_url
                 else:
@@ -134,8 +134,8 @@ def send_test_event(ip, login_account, login_password,eventid,message,severity):
                     payload["EventType"] = "Alert"
                     payload["EventTimestamp"] = timestamp
                     payload["Message"] = message
-                    payload["MessageArgs"] = []
-                    payload["MessageId"] = "Created"
+                    payload["MessageArgs"] = ["Lit","IndicatorLED"]
+                    payload["MessageId"] = "Base.1.5.PropertyValueNotInList"
                     payload["Severity"] = severity
                 response_send_event = REDFISH_OBJ.post(target_url, headers=headers, body=payload)
                 if response_send_event.status == 200 or response_send_event.status == 204:
@@ -143,6 +143,19 @@ def send_test_event(ip, login_account, login_password,eventid,message,severity):
                               + ",EventType:Alert,EventTimestamp:" + timestamp + ",Message:" + message \
                               + ",MessageArgs:[],MessageId:Created,Severity:" + severity\
                               + ",OriginOfCondition:" + event_url }
+                    return result
+                elif response_send_event.status == 202:
+                    task_uri = response_send_event.dict['@odata.id']
+                    result = utils.task_monitor(REDFISH_OBJ, task_uri)
+                    if result["ret"] is True and "Completed" == result["task_state"] and result['msg'] == '':
+                        REDFISH_OBJ.delete(task_uri, None)
+                    if result["ret"] is True:
+                        task_state = result["task_state"]
+                        if task_state == "Completed":
+                            result['msg'] = 'Send test event successfully. %s' % (result['msg'])
+                        else:
+                            result['ret'] = False
+                            result['msg'] = 'Send test event failed. %s' % (result['msg'])
                     return result
                 else:
                     error_message = utils.get_extended_error(response_send_event)
