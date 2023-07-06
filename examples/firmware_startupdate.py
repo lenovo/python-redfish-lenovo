0a2c9b14491eed8f3cd6def154ada2edd1bbf43d
Xiaojian XJ5 Ji <jixj5@Lenovo.com>
2023-07-06 16:28:35 +0800

    update copyright in firmware_startupdate.py

--- examples/firmware_startupdate.py
+++ examples/firmware_startupdate.py
@@ -4,7 +4,7 @@
 #
 # Copyright Notice:
 #
-# Copyright 2019 Lenovo Corporation
+# Copyright 2023 Lenovo Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
