33f4423e5f1fb7538c1a5d41117b8b20396f12de
renxulei <renxl2@lenovo.com>
2024-03-30 16:32:53 +0800

    Update update_bmc_user_account_type.py

--- examples/update_bmc_user_account_type.py
+++ examples/update_bmc_user_account_type.py
@@ -26,7 +26,7 @@ import json
 import traceback
 import lenovo_utils as utils
 
-def update_bmc_user_account_type(ip, login_account, login_password, username, account_type, account_url=None):
+def update_bmc_user_account_type(ip, login_account, login_password, username, account_type, new_password=None, account_url=None):
     """update user account type    
     :params ip: BMC IP address
     :type ip: string
@@ -38,6 +38,8 @@ def update_bmc_user_account_type(ip, login_account, login_password, username, ac
     :type username: string
     :params account_type: Account type to be modified by the user
     :type account_type: string
+    :params new_password: New password for the user specified when IPMI added in account type
+    :type new_password: string
     :params account_url: BMC account url
     :type account_url: string
     :returns: returns update user account type result when succeeded or error message when failed
@@ -56,92 +58,75 @@ def update_bmc_user_account_type(ip, login_account, login_password, username, ac
         result = {'ret': False, 'msg': "Please check the username, password, IP is correct\n"}
         return result
 
-    # change default BMC account's type or specified account (treat userid 1 as default account)
-    if username == None or account_url:
-        headers = {"If-Match": "*"}
-        parameter = {"AccountTypes": account_type}
-        default_account_url = '/redfish/v1/AccountService/Accounts/1'
-        if account_url:
-            default_account_url = account_url
-        response_modified_account_type = REDFISH_OBJ.patch(default_account_url, body=parameter, headers=headers)
+    # default user account url, treat userid 1 as default account
+    account_x_url = '/redfish/v1/AccountService/Accounts/1'
+    parameter = {"AccountTypes": account_type}
+    if "IPMI" in account_type:
+        if new_password == None:
+            result = {'ret': False, 'msg': "You must specify newpassword for the account you want to change because IPMI is specified in account type. \n"}
+            return result
+        parameter['Password'] = new_password
+    
+    if account_url != None:
+        account_x_url = account_url
+    
+    etag = ""
+    try:
+        # get the account url of username
+        if username != None:
+            # Get url accounts resource
+            accounts_url = '/redfish/v1/AccountService/Accounts'
+            response_accounts_url = REDFISH_OBJ.get(accounts_url, None)
+            if response_accounts_url.status == 200:
+                account_count = response_accounts_url.dict["Members@odata.count"]
+                # Loop the BMC user list and get all the bmc username
+                isUserExisted = False
+                for x in range(0, account_count):
+                    account_member_url = response_accounts_url.dict["Members"][x]["@odata.id"]
+                    response_account_member_url = REDFISH_OBJ.get(account_member_url, None)
+                    if response_account_member_url.status == 200:
+                        bmc_username = response_account_member_url.dict['UserName']
+                        # Update the BMC user account type when the specified BMC username is in the BMC user list.
+                        if bmc_username == username:
+                            if "@odata.etag" in response_account_member_url.dict:
+                                etag = response_account_member_url.dict['@odata.etag']
+                            else:
+                                etag = ""
+                            account_x_url = account_member_url 
+                            isUserExisted = True
+                            break   
+                    # account_member_url response failed
+                    else:
+                        try:
+                            error_message = utils.get_extended_error(response_account_member_url)
+                        except:
+                            error_message = response_account_member_url
+                        result = {'ret': False, 'msg': "response_account_member_url Error code %s \nerror_message: %s" % (
+                            response_account_member_url.status, error_message)}
+                        return result
+                
+                if isUserExisted == False:
+                    result = {'ret': False, 'msg': "BMC username specified doesn't exist. Please check whether the BMC username is correct."}
+                    return result
+            else:
+                error_message = utils.get_extended_error(response_accounts_url)
+                result = {'ret': False, 'msg': "Url '%s' response error code %s \nerror_message: %s" % (accounts_url, response_accounts_url.status, error_message)}
+                return result
+
+        headers = {"If-Match": etag}
+        response_modified_account_type = REDFISH_OBJ.patch(account_x_url, body=parameter, headers=headers)
         error_message = utils.get_extended_error(response_modified_account_type)
         if response_modified_account_type.status in [200,204]:
-            result = {'ret': True, 'msg': "The type of default BMC account is successfully updated."}
-            return result
-        elif response_modified_account_type.status == 400 and "The property AccountTypes is not in the list of valid properties" in error_message:
-            result = {'ret': False, 'msg': "Update default BMC account type failed, response error code %s \nerror_message: This platform does not support modifying the account type.\n" % (response_modified_account_type.status)}
+            if username != None:
+                msg = "Succeeded to update account type of BMC user %s. url is %s." % (username, account_x_url)
+            else: 
+                msg = "Succeeded to update account type of BMC user. url is %s." % account_x_url
+            result = {'ret': True, 'msg': msg}
             return result
         else:
-            result = {'ret': False, 'msg': "Update default BMC account type failed, response error code %s \nerror_message: %s\n" % (response_modified_account_type.status, error_message)}
+            result = {'ret': False, 'msg': "Failed to update account type of BMC user, url '%s' response error code %s \nerror_message: %s\n" % (account_x_url, response_modified_account_type.status, error_message)}
             return result
-
-    # change specified username account type
-    try:
-        # Get response_base_url resource
-        response_base_url = REDFISH_OBJ.get('/redfish/v1', None)
-
-        # Get account service url
-        if response_base_url.status == 200:
-            account_service_url = response_base_url.dict['AccountService']['@odata.id']
-        else:
-            error_message = utils.get_extended_error(response_base_url)
-            result = {'ret': False, 'msg': "Url '/redfish/v1' response Error code %s \nerror_message: %s" % (
-            response_base_url.status, error_message)}
-            return result
-
-        # Get AccountService resource
-        response_account_service_url = REDFISH_OBJ.get(account_service_url, None)
-        if response_account_service_url.status == 200:
-            accounts_url = response_account_service_url.dict['Accounts']['@odata.id']
-        else:
-            error_message = utils.get_extended_error(response_account_service_url)
-            result = {'ret': False, 'msg': "Url '%s' response Error code %s \nerror_message: %s" % (account_service_url, response_account_service_url.status, error_message)}
-            return result
-
-        # Get url accounts resource
-        response_accounts_url = REDFISH_OBJ.get(accounts_url, None)
-        if response_accounts_url.status == 200:
-            account_count = response_accounts_url.dict["Members@odata.count"]
-            # Loop the BMC user list and get all the bmc username
-            for x in range(0, account_count):
-                account_x_url = response_accounts_url.dict["Members"][x]["@odata.id"]
-                response_account_x_url = REDFISH_OBJ.get(account_x_url, None)
-                if response_account_x_url.status == 200:
-                    bmc_username = response_account_x_url.dict['UserName']
-                    # Update the BMC user account type when the specified BMC username is in the BMC user list.
-                    if bmc_username == username:
-                        if "@odata.etag" in response_account_x_url.dict:
-                            etag = response_account_x_url.dict['@odata.etag']
-                        else:
-                            etag = ""
-                        headers = {"If-Match": etag}
-                        parameter = {"AccountTypes": account_type}
-                        response_modified_account_type = REDFISH_OBJ.patch(account_x_url, body=parameter, headers=headers)
-                        error_message = utils.get_extended_error(response_modified_account_type)
-                        if response_modified_account_type.status in [200,204]:
-                            result = {'ret': True, 'msg': "The BMC user '%s' account type is successfully updated." % username}
-                            return result
-                        elif response_modified_account_type.status == 400 and "The property AccountTypes is not in the list of valid properties" in error_message:
-                            result = {'ret': False, 'msg': "Update default BMC account type failed, response error code %s \nerror_message: This platform does not support modifying the account type.\n" % (response_modified_account_type.status)}
-                            return result
-                        else:
-                            result = {'ret': False, 'msg': "Update BMC user account type failed, url '%s' response error code %s \nerror_message: %s\n" % (account_x_url, response_modified_account_type.status, error_message)}
-                            return result
-
-                # account_x_url response failed
-                else:
-                    try:
-                        error_message = utils.get_extended_error(response_account_x_url)
-                    except:
-                        error_message = response_account_x_url
-                    result = {'ret': False, 'msg': "response_account_x_url Error code %s \nerror_message: %s" % (
-                        response_account_x_url.status, error_message)}
-                    return result
-            result = {'ret': False, 'msg': "Specified BMC username doesn't exist. Please check whether the BMC username is correct."}
-        else:
-            error_message = utils.get_extended_error(response_accounts_url)
-            result = {'ret': False, 'msg': "Url '%s' response error code %s \nerror_message: %s" % (accounts_url,
-            response_accounts_url.status, error_message)}
+    
     except Exception as e:
         traceback.print_exc()
         result = {'ret':False, 'msg':"Error message %s" %e}
@@ -158,6 +143,7 @@ import argparse
 def add_helpmessage(argget):
     argget.add_argument('--username', type=str, help='Input the name of BMC user to be updated.')
     argget.add_argument('--accounttype', type=str, required=True, nargs="+", choices=["WebUI","Redfish","ManagerConsole","IPMI","SNMP"], help='Input account type of BMC user')
+    argget.add_argument('--newpassword', type=str, help='Input the password for the user specified, only needed when you specified "IPMI" in accounttype.')
     argget.add_argument('--url', type=str, help='Input account url of BMC user to update the account type.(e.g., /redfish/v1/AccountService/Accounts/2)')
 
 
@@ -170,6 +156,7 @@ def add_parameter():
         Change the account type for the specified user:
             "python update_bmc_user_account_type.py -i 10.10.10.10 -u USERID -p PASSW0RD --username USERNAME --accounttype WebUI Redfish"
             "python update_bmc_user_account_type.py -i 10.10.10.10 -u USERID -p PASSW0RD --accounttype WebUI Redfish --url /redfish/v1/AccountService/Accounts/2"
+            "python update_bmc_user_account_type.py -i 10.10.10.10 -u USERID -p PASSW0RD --accounttype WebUI Redfish IPMI --newpassword NEWPASSWORD --url /redfish/v1/AccountService/Accounts/2"
             """
     argget = utils.create_common_parameter_list(example_string=example)
     add_helpmessage(argget)
@@ -177,7 +164,9 @@ def add_parameter():
     parameter_info = utils.parse_parameter(args)
     parameter_info["username"] = args.username
     parameter_info["account_type"] = args.accounttype
+    parameter_info["new_password"] = args.newpassword
     parameter_info["account_url"] = args.url
+    
     return parameter_info
 
 
@@ -194,9 +183,10 @@ if __name__ == '__main__':
     username = parameter_info['username']
     account_type = parameter_info['account_type']
     account_url = parameter_info['account_url']
+    new_password = parameter_info["new_password"]
 
     # Update user account type result and check result   
-    result = update_bmc_user_account_type(ip, login_account, login_password, username, account_type, account_url)
+    result = update_bmc_user_account_type(ip, login_account, login_password, username, account_type, new_password, account_url)
     if result['ret'] is True:
         del result['ret']
         sys.stdout.write(json.dumps(result['msg'], sort_keys=True, indent=2) + '\n')
